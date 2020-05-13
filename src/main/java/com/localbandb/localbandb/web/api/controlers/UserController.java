64b0a6a1707e907863ed9075d7b131821c8756ab
sa6kok <sa6kok@abv.bg>
2020-05-13 21:27:22 +0200

    again

--- src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
+++ src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
@@ -13,12 +13,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 //@RequestMapping("/user")
 public class UserController extends BaseController {
     private final UserService userService;
