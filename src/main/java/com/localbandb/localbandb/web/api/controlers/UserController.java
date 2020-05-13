9f42629384b85c7b50ea0d30f25a51250f1c900d
sa6kok <sa6kok@abv.bg>
2020-05-13 04:01:39 +0200

    first commit

--- src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
+++ src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://**", maxAge = 3600)
 @RestController
 @RequestMapping("/user")
 public class UserController extends BaseController {
