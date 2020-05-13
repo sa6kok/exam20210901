6d1cf9dc0be3452b21dfa99e1eb94cdce32ca6a4
sa6kok <sa6kok@abv.bg>
2020-05-13 22:59:53 +0200

    again

--- src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
+++ src/main/java/com/localbandb/localbandb/web/api/controlers/UserController.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-//@RequestMapping("/user")
+@RequestMapping("/user")
 public class UserController extends BaseController {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
